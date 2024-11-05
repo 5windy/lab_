41069371c4028c7ba210aeae62fbe5fabc925530
5windy <5windy@naver.com>
2024-11-05 15:20:16 +0900

    Create ATM Project

---
+++ atm_1/src/atm/Atm.java
@@ -0,0 +1,12 @@
+package atm;
+
+public class Atm {
+	
+	public static void main(String[] args) {
+		
+		AtmSystem system = new AtmSystem();
+		system.run();
+		
+	}
+
+}
---
+++ atm_1/src/atm/AtmSystem.java
@@ -0,0 +1,11 @@
+package atm;
+
+public class AtmSystem {
+	
+	public void run() {
+//		- 회원가입/탈퇴)
+//		- 계좌신청/철회 (1인 3계좌까지)
+//		- 로그인
+	}
+
+}
