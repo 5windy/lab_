c8f69104326dce12b7d9374cb54667fa847b42c6
5windy <5windy@naver.com>
2024-11-05 15:25:41 +0900

    Add User private fields and getter

--- atm_1/src/atm/User.java
+++ atm_1/src/atm/User.java
@@ -1,5 +1,24 @@
 package atm;
 
+import java.util.ArrayList;
+
 public class User {
+	
+	private int code;
+	private String name;
+	private ArrayList<Account> accounts;
+	
+	public User(int code, String name) {
+		this.code = code;
+		this.name = name;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 
 }
