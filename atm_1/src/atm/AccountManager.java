b21d27bf63a1b3fc8d7c713581b300d12bad24c9
5windy <5windy@naver.com>
2024-11-07 12:36:07 +0900

    Add methods : Account CRD

--- atm_1/src/atm/AccountManager.java
+++ atm_1/src/atm/AccountManager.java
@@ -1,6 +1,7 @@
 package atm;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class AccountManager {
 	
@@ -13,5 +14,68 @@ public class AccountManager {
 	}
 	
 	private ArrayList<Account> list = new ArrayList<>();
+	
+	// Account CRD
+	
+	public Account addAccount(int userCode, int password) {
+		// 1. 유저 조회 후, (시스템)
+		// 2. 잔여 계좌개설 가능 수 확인 (시스템) 
+		// 3. 개좌 개설
+		int code = generateAccountCode();
+		Account account = new Account(code, userCode, password);
+		
+		list.add(account);
+		return account;
+	}
+
+	private int generateAccountCode() {
+		int code = 0;
+		
+		Random random = AtmSystem.random;
+		while(true) {
+			code = random.nextInt(9000) + 1000;
+			
+			Account target = new Account(code);
+			if(!list.contains(target))
+				break;
+		}
+		return code;
+	}
+	
+	public Account getAccountByCode(int code) {
+		Account target = new Account(code);
+		int index = list.indexOf(target);
+		
+		if(index == -1)
+			return null;
+		
+		return list.get(index);
+	}
+	
+	public ArrayList<Account> getAccountByUserCode(int userCode) {
+		ArrayList<Account> list = new ArrayList<>();
+		
+		for(int i=0; i<this.list.size(); i++) {
+			Account account = this.list.get(i);
+			if(account.getUserCode() == userCode)
+				list.add(account);
+		}
+		return list;
+	}
+	
+	public boolean removeAccountByCode(int code, int password) {
+		Account target = new Account(code);
+		int index = list.indexOf(target);
+		
+		if(index == -1)
+			return false;
+		
+		Account account = list.get(index);
+		if(!account.isValidPassword(password))
+			return false;
+		
+		list.remove(account);
+		return true;
+	}
 	
 }
