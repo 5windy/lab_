47b4fe6054242ec21436b6ea9bea258abbc0eb61
5windy <5windy@naver.com>
2024-11-07 10:17:45 +0900

    Add Account fields and getter & setter

--- atm_1/src/atm/Account.java
+++ atm_1/src/atm/Account.java
@@ -1,5 +1,39 @@
 package atm;
 
 public class Account {
+	
+	private int code;
+	private int userCode;
+	private int password;
+	private int balance;
+	
+	public Account(int code, int userCode, int password) {
+		this.code = code;
+		this.userCode = userCode;
+		this.password = password;
+	}
+	
+	public Account(int code, int userCode, int password, int balance) {
+		this.code = code;
+		this.userCode = userCode;
+		this.password = password;
+		this.balance = balance;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+	public int getUserCode() {
+		return this.userCode;
+	}
+	
+	public int getBalance() {
+		return this.balance;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
 
 }
