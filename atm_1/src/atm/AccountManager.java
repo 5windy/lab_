3af979a6676ca77d6229cf41c2272f8d6b37c62d
5windy <5windy@naver.com>
2024-11-07 11:07:07 +0900

    Add AccountManager

---
+++ atm_1/src/atm/AccountManager.java
@@ -0,0 +1,17 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class AccountManager {
+	
+	private AccountManager() {
+		
+	}
+	private static AccountManager instance = new AccountManager();
+	public static AccountManager getInstance() {
+		return instance;
+	}
+	
+	private ArrayList<Account> list = new ArrayList<>();
+	
+}
